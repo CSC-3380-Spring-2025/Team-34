ad8441eab52aba59c3b56531eaae4bcc12db5626
fschaf2 <fschaf2@lsu.edu>
2025-03-20 21:43:57 -0500

    Restructured in preparation for adding to main database

--- src/datastore/create_multi_department_data.py
+++ src/datastore/create_multi_department_data.py
@@ -1,9 +1,21 @@
-import parse_courses as pc
+from src.datastore import parse_courses as pc
+
 
 def collect_multi_department_data(links):
     full_data=list[dict[str, str]]
     for link in links:
         full_data.extend(pc.parse_page(link))
-    pc.create_csv(full_data,'multi_department_data')
+    return full_data
 
-links=['https://appl101.lsu.edu/booklet2.nsf/All/67FD57ECBF3676C486258BAC002C42AB?OpenDocument', 'https://appl101.lsu.edu/booklet2.nsf/All/2719C3AEB8F7AE3986258BAC002C42D6?OpenDocument']
+def make_multi_csv(links):
+    pc.create_csv(collect_multi_department_data(links), 'multi_department_data')
+
+def make_multi_parquet(links):
+    pc.create_parquet(collect_multi_department_data(links), 'multi_department_data')
+
+def collect_default_data():
+    links = ['https://appl101.lsu.edu/booklet2.nsf/All/67FD57ECBF3676C486258BAC002C42AB?OpenDocument',
+             'https://appl101.lsu.edu/booklet2.nsf/All/2719C3AEB8F7AE3986258BAC002C42D6?OpenDocument']
+    create_multi_department_data(links)
+
+make_multi_csv(collect_default_data())
