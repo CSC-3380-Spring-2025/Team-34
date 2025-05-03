75fce32db0011b8b5b025e85114b5e184e9dfb22
Felix Schafer <fschaf2@lsu.edu>
2025-05-03 03:56:51 +0000

    swapped which axes is which to better suit lsu data

--- src/test.py
+++ src/test.py
@@ -693,8 +693,8 @@ with st.container():
             st.subheader("Visualize Data")
             numerical_cols = df.select_dtypes(include=["number"]).columns
             if len(numerical_cols) > 1:
-                x_axis = st.selectbox("Select X-Axis:", numerical_cols, key="x_axis")
-                y_axis = st.selectbox("Select Y-Axis:", numerical_cols, index=1, key="y_axis")
+                x_axis = st.selectbox("Select X-Axis:", numerical_cols, index=1, key="x_axis")
+                y_axis = st.selectbox("Select Y-Axis:", numerical_cols, index=0, key="y_axis")
                 fig = px.scatter(df, x=x_axis, y=y_axis, title=f"{x_axis} vs {y_axis}")
                 st.plotly_chart(fig, use_container_width=True)
             elif len(numerical_cols == 1):
