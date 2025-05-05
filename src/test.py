53f08a31780e4b7953b876ba3dd720d09de38a52
bdogdavis <bdav213@lsu.edu>
2025-05-05 13:55:54 -0700

    Update test.py

--- src/test.py
+++ src/test.py
@@ -554,7 +554,7 @@ def render_sidebar() -> None:
                 st.session_state.logged_in = False
                 st.session_state.username = None
                 st.session_state.show_lsu_datastore = False
-                main()
+                st.rerun()
 
 def render_blank_page() -> None:
     """Render the Blank Page for viewing CSV files."""
